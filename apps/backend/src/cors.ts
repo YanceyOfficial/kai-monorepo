28fbda5460844b86eac383d31f364e9ad7e360a3
YanceyOfficial <yanceyofficial@gmail.com>
2024-06-24 13:08:13 +0800

    build: support cors

---
+++ apps/backend/src/cors.ts
@@ -0,0 +1,11 @@
+const CORS_ORIGINS_PRODUCTION = [/\.?yancey\.app$/]
+const CORS_ORIGINS_DEVELOPMENT = ['http://localhost:5173']
+
+export const configCORS = (isEnvProduction: boolean) => ({
+  origin: isEnvProduction ? CORS_ORIGINS_PRODUCTION : CORS_ORIGINS_DEVELOPMENT,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: '*'
+})
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,19 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import { configCORS } from './cors'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule, {
+    cors: configCORS(process.env.NODE_ENV === 'production')
+  })
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Kai Services')
+    .setDescription('The kai API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('kai')
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
 
-  await app.listen(process.env.PORT || 3002)
+  await app.listen(process.env.PORT || 2998)
 }
 bootstrap()
