77470d60ca7461986f7294672e5fb0962658c323
YanceyOfficial <yanceyofficial@gmail.com>
2024-06-24 12:44:31 +0800

    build: pm2 deployment

---
+++ apps/backend/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'kai-be',
+      script: './dist/main.js',
+      watch: ['./dist/'],
+      ignore_watch: ['node_modules'],
+      autorestart: true,
+      max_memory_restart: '1G',
+      instances: 2,
+      exec_mode: 'cluster',
+      env: { NODE_ENV: 'production' }
+    }
+  ]
+}
