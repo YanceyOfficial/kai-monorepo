206e6d0580cca060a8c2ae81f20ea1b8c7d53bb8
YanceyOfficial <yanceyofficial@gmail.com>
2024-06-27 22:01:27 +0800

    ci: update ci

--- apps/backend/src/guard/types.ts
+++ apps/backend/src/guard/types.ts
@@ -25,11 +25,11 @@ export interface Claims {
   resource_access: ResourceAccess
   scope: string
   sid: string
+  email: string
   email_verified: boolean
   name: string
   preferred_username: string
   given_name: string
   family_name: string
-  email: string
   'allowed-origins': string[]
 }
