0494dbd54b978dd45c9732c39de98f9d5898b32a
YanceyOfficial <yanceyofficial@gmail.com>
2024-09-09 13:00:53 +0800

    feat: switch to ielts_word table temporarily

--- apps/backend/src/word/word.module.ts
+++ apps/backend/src/word/word.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { WordController } from './word.controller'
-import { Word, WordSchema } from './word.schema'
+import { WordSchema } from './word.schema'
 import { WordService } from './word.service'
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Word.name, schema: WordSchema }])
+    // MongooseModule.forFeature([{ name: Word.name, schema: WordSchema }])
+    // MongooseModule.forFeature([{ name: 'toeic_word', schema: WordSchema }])
+    MongooseModule.forFeature([{ name: 'ielts_word', schema: WordSchema }])
   ],
   controllers: [WordController],
   providers: [WordService]
